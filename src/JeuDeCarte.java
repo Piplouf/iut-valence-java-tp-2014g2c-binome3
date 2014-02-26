f63117a4ec87fd979550ce5dde992ca114d86ace
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 23:56:56 +0100

    Add TODO (JeuDeCarte.java)

--- src/JeuDeCarte.java
+++ src/JeuDeCarte.java
@@ -2,45 +2,40 @@
 
 public class JeuDeCarte {
 
-	private Carte carte[] = new Carte[52];
-	
-	private int nombreCartesRestantes;
-	
+    private Carte carte[] = new Carte[52];
 
-	public JeuDeCarte(){
-		int i = 0;
-		for(Couleur coul : Couleur.values()){
-			for(Numero num : Numero.values()){
-				carte[i++] = new Carte(coul,num,false);
-			}
-		}
-		this.nombreCartesRestantes = i++;
-	}
-	
-	
-	
-	public Carte[] distribuerCartePile(int nombreCartesPile) {
-		/* Pr�-condition sur la taille de la demande */
-		
-		Carte[] carteARetourner = new Carte[nombreCartesPile];
-		
-		if(nombreCartesPile < 0 || nombreCartesPile <= this.nombreCartesRestantes){}
-		else
-		{
-		
-		
-		for(int i=0;i<nombreCartesPile;i++)
-		{
-			int rand = 0 + (int)(Math.random() * ((51) + 1));
-			carteARetourner[i] = this.carte[rand];
-			for(i = rand;i<nombreCartesRestantes;i++)
-			{
-				this.carte[rand] = this.carte[rand + 1];
-			}
-			this.nombreCartesRestantes--;
-		}
+    private int nombreCartesRestantes;
 
-		}
-		return carteARetourner;
-	}
+    public JeuDeCarte() {
+        nombreCartesRestantes = 0;
+        for (Couleur coul : Couleur.values()) {
+            for (Numero num : Numero.values()) {
+                carte[nombreCartesRestantes++] = new Carte(coul, num, false);
+            }
+        }
+    }
+
+    public Carte[] distribuerCartePile(int nombreCartesPile) {
+        /* Pr�-condition sur la taille de la demande */
+
+        Carte[] carteARetourner = new Carte[nombreCartesPile];
+        /* TODO Votre choix d'implémentation est (très) discutable. */
+        if (nombreCartesPile < 0 || nombreCartesPile <= this.nombreCartesRestantes) {
+
+        }
+        else {
+            for (int i = 0; i < nombreCartesPile; i++) {
+                /* XXX Pourquoi 51 ? */
+                /* XXX Pourquoi 0+ ? */
+                int rand = 0 + (int) (Math.random() * ((51) + 1));
+                carteARetourner[i] = this.carte[rand];
+                /* XXX Pourquoi i ? */
+                for (i = rand; i < nombreCartesRestantes; i++) {
+                    this.carte[rand] = this.carte[rand + 1];
+                }
+                this.nombreCartesRestantes--;
+            }
+        }
+        return carteARetourner;
+    }
 }

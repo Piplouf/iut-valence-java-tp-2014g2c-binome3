b80e5412eb8253a9458f27d727157e63ee25b2ca
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 23:58:11 +0100

    Add TODO (Solitaire.java)

--- src/Solitaire.java
+++ src/Solitaire.java
@@ -2,11 +2,7 @@
 public class Solitaire {
 
 	public static void main(String[] args) {
-	 /* TODO Instanciation de votre jeu */
-		
-		Partie partie1 = new Partie();
-		
-		partie1.afficherEtatPartie();
-		
+		Partie partie = new Partie();
+		partie.afficherEtatPartie();
 	}
 }
